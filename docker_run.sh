e5230588b020d52c2bd8855abff28250c4148047
awb99 <andreas.wolfgang.bauer@gmail.com>
2016-03-15 18:47:34 +0100

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 docker run -i -t -p 8888:8888 -v \
 ./csharp_notebooks:/home/condauser/notebooks ic1 \
-/home/condauser/anaconda3/bin/ipython notebook --profile=icsharp --Session.key='' --Session.keyfile=''
+/home/condauser/anaconda3/bin/ipython notebook --profile=icsharp --Session.key='' --Session.keyfile='' --ip=*
