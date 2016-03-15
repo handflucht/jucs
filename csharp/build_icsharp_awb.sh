e06c45ce2ff791b1a11adcd96dfddf6ff86bf9c5
awb99 <andreas.wolfgang.bauer@gmail.com>
2016-03-15 16:36:14 +0100

    Update build_icsharp_awb.sh

--- csharp/build_icsharp_awb.sh
+++ csharp/build_icsharp_awb.sh
@@ -1,7 +1,8 @@
- # without mozroot http certificates nuget does not work
- mozroots --import --sync
+. /opt/mono/env.sh
+# without mozroot http certificates nuget does not work
+mozroots --import --sync
  
- cd /home/condauser/icsharp 
- mkdir /home/condauser/icsharp/packages 
- mono ./.nuget/NuGet.exe restore ./icSharp.sln 
- xbuild ./iCSharp.sln /property:Configuration=Release /nologo /verbosity:normal
+cd /home/condauser/icsharp 
+mkdir /home/condauser/icsharp/packages 
+mono ./.nuget/NuGet.exe restore ./icSharp.sln 
+xbuild ./iCSharp.sln /property:Configuration=Release /nologo /verbosity:normal
