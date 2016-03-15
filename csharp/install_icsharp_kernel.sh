b44b019625d6af02a89427e643ecb8b84010cd57
awb99 <andreas.wolfgang.bauer@gmail.com>
2016-03-15 17:18:22 +0100

    Update install_icsharp_kernel.sh

--- csharp/install_icsharp_kernel.sh
+++ csharp/install_icsharp_kernel.sh
@@ -1,2 +1,4 @@
+mkdir  /usr/local/share/jupyter
+mkdir  /usr/local/share/jupyter/kernels
 mkdir  /usr/local/share/jupyter/kernels/icsharp
 mv /tmp/kernel.json /usr/local/share/jupyter/kernels/icsharp
